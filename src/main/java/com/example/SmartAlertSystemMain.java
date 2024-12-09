e05043e5f5d4bb4ce2053f13c9dc7e1739a04637
AMAN JAIN <98209921+Aman23000@users.noreply.github.com>
2024-12-09 18:42:44 -0500

    Updated Code

--- src/main/java/com/example/SmartAlertSystemMain.java
+++ src/main/java/com/example/SmartAlertSystemMain.java
@@ -1,6 +1,6 @@
 package com.example;
 
-public class SmartAlertSystem {
+public class SmartAlertSystemMain {
     public static void main(String[] args) {
         EventManager eventManager = new EventManager();
         NotificationManager notificationManager = new NotificationManager();
